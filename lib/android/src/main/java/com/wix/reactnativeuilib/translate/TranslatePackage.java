8d37bda75b8c603bde830009723bfb2fd9bf4df0
omerwin <omerwin@wix.com>
2018-11-12 13:51:49 +0200

    Add the ability to use native module to extract country names by user language

--- lib/android/src/main/java/com/wix/reactnativeuilib/translate/TranslatePackage.java
+++ lib/android/src/main/java/com/wix/reactnativeuilib/translate/TranslatePackage.java
@@ -1,7 +1,6 @@
 package com.wix.reactnativeuilib.translate;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -19,11 +18,6 @@ public class TranslatePackage implements ReactPackage {
         modules.add(new TranslateManager(reactContext));
 
         return modules;
-    }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
     }
 
     @Override
