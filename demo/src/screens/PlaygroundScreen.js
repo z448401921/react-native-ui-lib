dad54e46c94645949611282d32f972d243864053
Ethan Sharabi <ethans@wix.com>
2018-12-26 17:40:14 +0200

    create a demo for testing performance of baseComponents (modifiers)

--- demo/src/screens/PlaygroundScreen.js
+++ demo/src/screens/PlaygroundScreen.js
@@ -1,20 +1,79 @@
 import React, {Component} from 'react';
-import {StyleSheet} from 'react-native';
+import _ from 'lodash';
+import {StyleSheet, View as RNView, Text as RNText} from 'react-native';
 import {Colors, Constants, View, Text, Button, Modal} from 'react-native-ui-lib'; //eslint-disable-line
 
+const Profiler = React.unstable_Profiler;
+
 export default class PlaygroundScreen extends Component {
-  
   constructor(props) {
     super(props);
     this.state = {};
   }
 
-  componentDidMount() {}
+  measurements = [];
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.clearMeasurements();
+    }, 1000);
+  }
+
+  clearMeasurements = () => {
+    this.measurements.push(this.state.uilib.baseDuration / this.state.react.baseDuration);
+    this.setState({uilib: undefined, react: undefined});
+    if (this.measurements.length >= 10) {
+      clearInterval(this.interval);
+    }
+  };
+
+  logMeasurement = async (id, phase, actualDuration, baseDuration) => {
+    if (!this.state[id]) {
+      // see output during DEV
+      console.log({id, phase, actualDuration, baseDuration});
+      this.setState({[id]: {phase, actualDuration, baseDuration}});
+    }
+  };
+
+  renderUilib() {
+    return (
+      <Profiler id={'uilib'} onRender={this.logMeasurement}>
+        <View flex center _style={styles.container}>
+          {/* <Text>UILIB VIEW</Text> */}
+        </View>
+      </Profiler>
+    );
+  }
+
+  renderReactNative() {
+    return (
+      <Profiler id={'react'} onRender={this.logMeasurement}>
+        <RNView>{/* <RNText>RN VIEW</RNText> */}</RNView>
+      </Profiler>
+    );
+  }
+
+  renderDifference() {
+    const {uilib, react} = this.state;
+    if (uilib && react) {
+      return (
+        <View flex center>
+          <Text text30>{this.measurements.length}</Text>
+          <Text text30>{_.sum(this.measurements) / this.measurements.length}</Text>
+        </View>
+      );
+    }
+  }
 
   render() {
     return (
-      <View flex center style={styles.container}>
-        <Text>Unicorn Playground Screen</Text>
+      <View flex>
+        {this.renderReactNative()}
+        {this.renderUilib()}
+        {this.renderDifference()}
+        <View flex center>
+          <Button label="Clear" onPress={this.clearMeasurements} />
+        </View>
       </View>
     );
   }
